f507441bf48218a1dfd84c4d2000c20dd68f7a41
Randriamampionona <tojorandriaII474@gmail.com>
2023-03-08 20:00:49 +0300

    fix slide UX

--- components/common/sectionTitle/SectionTitle.jsx
+++ components/common/sectionTitle/SectionTitle.jsx
@@ -3,12 +3,16 @@ import React from "react";
 const SectionTitle = ({ title }) => {
 	return (
 		<div className="w-full my-10">
-			<h1 className="text-xl md:text-2xl text-center font-medium">
+			<h1 className="text-xl md:text-2xl text-center font-medium uppercase">
 				{title}
 			</h1>
 			<div className="mt-[7px] mx-auto w-14 h-[3px] bg-primaryColor" />
 		</div>
 	);
+};
+
+SectionTitle.defaultProps = {
+	title: "Title here",
 };
 
 export default SectionTitle;
