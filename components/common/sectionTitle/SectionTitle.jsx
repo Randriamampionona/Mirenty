f1623cf24dfd228a5627a0f81f26b3f915747c1a
Randriamampionona <tojorandriaII474@gmail.com>
2023-03-08 20:24:29 +0300

    fix title section - add desc

--- components/common/sectionTitle/SectionTitle.jsx
+++ components/common/sectionTitle/SectionTitle.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
-const SectionTitle = ({ title }) => {
+const SectionTitle = ({ title, desc }) => {
 	return (
 		<div className="w-full my-10">
-			<h1 className="text-xl md:text-2xl text-center font-medium uppercase">
-				{title}
-			</h1>
-			<div className="mt-[7px] mx-auto w-14 h-[3px] bg-primaryColor" />
+			<div className="mx-auto">
+				<h1 className="text-xl md:text-2xl text-center font-medium uppercase">
+					{title}
+				</h1>
+				<div className="mt-[7px] mx-auto w-14 h-[3px] bg-primaryColor" />
+			</div>
+
+			<p className="text-center max-w-md my-6 text-gray-700">{desc}</p>
 		</div>
 	);
 };
 
 SectionTitle.defaultProps = {
 	title: "Title here",
+	desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis optio cumque beatae ipsa voluptate! Exercitationem, soluta nobis aliquid eligendi ullam laboriosam illum praesentium blanditiis suscipit, error impedit aut qui laborum!",
 };
 
 export default SectionTitle;
